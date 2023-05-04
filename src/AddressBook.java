09f57b21f520123feddf20caea6787a7ba483a48
Amir Ullah <amir-ullah41@outlook.com>
2023-05-04 15:10:00 +0100

    Created the AddressBook Class and the Contact class. Which will be used to get the information of the contact when added by the user.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,50 @@
+import java.util.ArrayList;
+
 
 public class AddressBook {
-    public static void main(String[] args) {
+    private ArrayList<Contact> contacts;
 
+    public AddressBook() {
+        this.contacts = new ArrayList<Contact>();
+    }
+
+    public void addContact(Contact contact){
+        this.contacts.add(contact);
+    }
+
+    public void updateContact(Contact contact) {
+        int index = findContactIndex(contact);
+        if (index != -1) {
+            contacts.set(index, contact);
+        }
+    }
+
+    public void deleteContact(Contact contact) {
+        int index = findContactIndex(contact);
+        if (index != -1) {
+            contacts.remove(index);
+        }
+    }
+
+    public Contact searchContact(String searchTerm) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(searchTerm) || contact.getPhoneNumber().equals(searchTerm)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    private int findContactIndex(Contact contact) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).equals(contact)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
     }
 }
---
+++ src/Contact.java
@@ -0,0 +1,40 @@
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Contact(String name, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + phoneNumber + " - " + emailAddress;
+    }
+}
